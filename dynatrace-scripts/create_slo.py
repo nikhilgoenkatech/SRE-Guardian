c91f2b88dd9b1d33a2e5f899050092dcb1e87888
nikhilgoenkatech <52188007+nikhilgoenkatech@users.noreply.github.com>
2022-12-02 12:12:27 +1100

    Update create_slo.py

--- dynatrace-scripts/create_slo.py
+++ dynatrace-scripts/create_slo.py
@@ -58,18 +58,96 @@ def create_slo(DT_URL, DT_TOKEN):
     "timeframe": "-1d"
   }
 
+  #Database Response time SLO definition  
+  DB_RSP_PAYLOAD = {
+    "enabled": "true",
+    "name": "$3 - DB Response Time SLO",
+    "description": "Datbase Response Time SLO",
+    "metricName": "db_response_time_$5",
+    "metricExpression":"((builtin:service.response.time:filter(in(\"dt.entity.service\",entitySelector(\"type(~\"SERVICE~\")\"))):filter(in(\"dt.entity.service\",entitySelector(\"type(SERVICE),entityName.equals(~\"test~\")\"))):avg:toUnit(MicroSecond,MilliSecond):partition(latency,value(good,lt(36.17))):splitBy():count:fold)/(builtin:service.response.time:filter(in(\"dt.entity.service\",entitySelector(\"type(~\"SERVICE~\")\"))):filter(in(\"dt.entity.service\",entitySelector(\"type(SERVICE),entityName.equals(~\"test~\")\"))):avg:toUnit(MicroSecond,MilliSecond):splitBy():count:fold)*(100))",
+    "evaluationType": "AGGREGATE",
+    "filter": "type(\"SERVICE\")",
+    "target": 95,
+    "warning": 97.5,
+    "errorBudgetBurnRate": {
+      "fastBurnThreshold": 1.5,
+      "burnRateVisualizationEnabled": "true"
+    },
+    "timeframe": "-1d"
+  }
+  
+  #DB Response Rate SLO definition  
+  DB_FAILURE_PAYLOAD = {
+    "enabled": "true",
+    "name": "$3 - Database Failure Rate",
+    "description": "DB Failure Rate",
+    "metricName": "db_failure_rate_$5",
+    "metricExpression":"(100)*(builtin:service.errors.total.successCount:filter(in(\"dt.entity.service\",entitySelector(\"type(~\"SERVICE~\")\"))):filter(in(\"dt.entity.service\",entitySelector(\"type(SERVICE),entityName.equals(~\"test~\")\"))):splitBy())/(builtin:service.requestCount.total:filter(in(\"dt.entity.service\",entitySelector(\"type(~\"SERVICE~\")\"))):filter(in(\"dt.entity.service\",entitySelector(\"type(SERVICE),entityName.equals(~\"test~\")\"))):splitBy())",
+    "evaluationType": "AGGREGATE",
+    "filter": "type(\"SERVICE\")",
+    "target": 95,
+    "warning": 97.5,
+    "errorBudgetBurnRate": {
+      "fastBurnThreshold": 1.5,
+      "burnRateVisualizationEnabled": "true"
+    },
+    "timeframe": "-1d"
+  }
+
+  #Application Performance SLO definition  
+  APP_RSP_PAYLOAD = {
+    "enabled": "true",
+    "name": "$3 - App Response Time SLO",
+    "description": "Application Response Time SLO",
+    "metricName": "app_response_time_$5",
+    "evaluationType": "AGGREGATE",
+    "metricExpression":"(100)*((builtin:apps.web.actionCount.category:filter(in(\"dt.entity.application\",entitySelector(\"type(APPLICATION)\"))):filter(eq(\"Apdex category\",SATISFIED)):splitBy())/(builtin:apps.web.actionCount.category:filter(in(\"dt.entity.application\",entitySelector(\"type(APPLICATION)\"))):splitBy()))",
+    "filter": "type(\"APPLICATION\")",
+    "target": 95,
+    "warning": 97.5,
+    "errorBudgetBurnRate": {
+      "fastBurnThreshold": 1.5,
+      "burnRateVisualizationEnabled": "true"
+    },
+    "timeframe": "-1d"
+  }
+
+  #APP Response Rate SLO definition  
+  APP_FAILURE_PAYLOAD = {
+    "enabled": "true",
+    "name": "$3 - App Failure Rate",
+    "description": "App Failure Rate",
+    "metricName": "app_failure_rate_$5",
+    "metricExpression":"(100)*((builtin:apps.web.countOfErrorsDuringUserActions:filter(in(\"dt.entity.application\",entitySelector(\"type(APPLICATION)\"))))/(builtin:apps.web.actionCount.category:filter(in(\"dt.entity.application\",entitySelector(\"type(APPLICATION)\"))):splitBy()))",
+    "evaluationType": "AGGREGATE",
+    "filter": "type(\"APPLICATION\")",
+    "target": 95,
+    "warning": 97.5,
+    "errorBudgetBurnRate": {
+      "fastBurnThreshold": 1.5,
+      "burnRateVisualizationEnabled": "true"
+    },
+    "timeframe": "-1d"
+  }
+
+
   try:
     data = {}
 
     query = str(DT_URL) + "/api/v2/slo/"
     post_param = {'Content-Type':'application/json', 'Authorization':'Api-Token {}'.format(DT_TOKEN)}
 
+    #Create Service SLOs
     populate_data = requests.post(query, headers = post_param, data = json.dumps(populate_payload(RSP_PAYLOAD)))
-    print(populate_data.status_code)
-
     populate_data = requests.post(query, headers = post_param, data = json.dumps(populate_payload(FAILURE_PAYLOAD)))
-    print(json.dumps(populate_payload(FAILURE_PAYLOAD)))
-    print(populate_data.status_code)
+    
+    #Create Database SLOs
+    populate_data = requests.post(query, headers = post_param, data = json.dumps(populate_payload(DB_RSP_PAYLOAD)))
+    populate_data = requests.post(query, headers = post_param, data = json.dumps(populate_payload(DB_FAILURE_PAYLOAD)))
+    
+    #Create Application SLOs
+    populate_data = requests.post(query, headers = post_param, data = json.dumps(populate_payload(APP_RSP_PAYLOAD)))
+    populate_data = requests.post(query, headers = post_param, data = json.dumps(populate_payload(APP_FAILURE_PAYLOAD)))
 
     if populate_data.status_code == 401:
       msg = "Auth Error"
